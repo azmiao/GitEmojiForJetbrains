13f81b3e37a87760afe9fbe5d8e0669938771534
azmiao <2362020227@qq.com>
2026-08-20 12:48:10 +0800

    feat ✨: 项目骨架与 Gradle 配置

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    java
+    kotlin("jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
+}
+
+group = "com.gitmoji.plugin"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// 配置 JDK 21 路径
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        create("IC", "2024.2")
+        bundledPlugin("Git4Idea")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "com.gitmoji.plugin"
+        name = "GitEmojiForJetbrains"
+        version = "1.0.0"
+        ideaVersion {
+            sinceBuild = "242"
+            untilBuild = null
+        }
+        description = "Git Commit Emoji Prefix Plugin"
+        vendor {
+            name = "GitEmoji"
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks {
+    compileKotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
+    }
+    compileJava {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
