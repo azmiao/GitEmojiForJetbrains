92e72673dce3f2b9bdf1a0ce6684d4669284fe5e
azmiao <2362020227@qq.com>
2026-08-20 13:04:00 +0800

    feat ✨: 格式化引擎与占位符替换

---
+++ src/main/kotlin/com/gitmoji/plugin/FormatEngine.kt
@@ -0,0 +1,18 @@
+package com.gitmoji.plugin
+
+object FormatEngine {
+
+    private val PLACEHOLDER_PATTERN = Regex("""\$\{(\w+)\}""")
+
+    fun format(formatTemplate: String, emojiTemplate: EmojiTemplate): String {
+        return PLACEHOLDER_PATTERN.replace(formatTemplate) { match ->
+            when (match.groupValues[1]) {
+                "emoji" -> emojiTemplate.emoji
+                "type" -> emojiTemplate.type
+                "name" -> emojiTemplate.name
+                "description" -> emojiTemplate.description
+                else -> match.value
+            }
+        }
+    }
+}
